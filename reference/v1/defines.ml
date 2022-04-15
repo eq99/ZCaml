a93ebd873075369f2dd206b3f496dcefac9e54e6
xiayulu <harolddfinchh@gmail.com>
2022-04-15 22:52:55 +0800

    doc

--- reference/v1/defines.ml
+++ reference/v1/defines.ml
@@ -2,3 +2,6 @@ let a =1 and b = 2;;
 
 
 (** [even n] is whether [n] is even. Requires: [n >= 0]. *) let rec even n = n = 0 || odd (n - 1)  and (** [odd n] is whether [n] is odd.(***) Requires: [n >= 0]. *) odd n = n <> 0 && even (n - 1);;
+
+
+let rec even n = n = 0 || odd (n - 1) and odd n = n <> 0 && even (n - 1);;
